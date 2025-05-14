e7851832f7bf7f9c5eeb23fedf4988c6f0244129
m4md24 <m4md24@gmail.com>
2025-05-14 06:04:32 +0300

    # Learn Access Specifiers and Modifiers

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
     2. [Classes and Objects](src/_1_concepts/_1_2_classes_and_objects)
     3. Class Members
     4. Objects in Memory
+    5. [Access Specifiers and Modifiers](src/_1_concepts/_1_5_access_specifiers_and_modifiers)
---
+++ src/_1_concepts/_1_5_access_specifiers_and_modifiers/AccessSpecifiersAndModifiers.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Person {
+    int variable1 = 1;
+    int function1() { return 1; }
+
+protected:
+    int variable2 = 2;
+    int function2() { return 2; }
+
+public:
+    string firstName,
+           secondName;
+    string fullName() { return firstName + " " + secondName; }
+    int function3() { return variable1 + variable2; }
+};
+
+int main() {
+    Person person;
+    person.firstName = "Mohamed";
+    person.secondName = "Sadawy";
+    cout << "Full Name: " << person.fullName() << endl;
+    cout << "Function 3: " << person.function3();
+}
