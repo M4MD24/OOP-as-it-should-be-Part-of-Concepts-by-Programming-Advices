200f3e02b47a92699495b5e791d50c914a1eeeba
m4md24 <m4md24@gmail.com>
2025-05-14 06:01:19 +0300

    # Learn Classes and Objects

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. Concepts
     1. OOP
+    2. [Classes and Objects](src/_1_concepts/_1_2_classes_and_objects)
---
+++ src/_1_concepts/_1_2_classes_and_objects/ClassesAndObjects.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Person {
+public:
+    string firstName,
+           secondName;
+    string fullName() { return firstName + " " + secondName; }
+};
+
+int main() {
+    Person person;
+    person.firstName = "Mohamed";
+    person.secondName = "Sadawy";
+    cout << person.fullName();
+
+    string text;
+}
